5051a5716be91650dc148d5df6e707de29a905d4
Arman Mia <mdarman365506@gmail.com>
2025-10-14 15:55:19 +0600

    added basic project setup with express

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import compression from "compression";
+import cors from "cors";
+import express from "express";
+import path from "path";
+
+const app = express();
+
+// Middleware
+
+app.use(compression()); // Compresses response bodies for faster delivery
+app.use(express.json()); // Parse incoming JSON requests
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+
+// Home route
+app.get("/", (_req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+
+
+// 404 Handler
+app.use((_req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+  });
+});
+
+export default app;
