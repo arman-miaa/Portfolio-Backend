8566bce3accd3ae81c9bde18097dcac74fb9df7b
Arman Mia <mdarman365506@gmail.com>
2025-10-22 18:19:00 +0600

    Added ContactMessage model with service, controller, and routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import { blgoRoute } from "../modules/blog/blogs.routes";
 import { projectroute } from "../modules/projects/projects.routes";
 import { skillRoute } from "../modules/skill/skill.routes";
 import { contactRoute } from "../modules/contact/contact.routes";
+import { contactMessageRoute } from "../modules/contactMessage/contactMessage.route";
 
 
 export const router = Router();
@@ -28,6 +29,10 @@ const moduleRoutes = [
   {
     path: "/contact",
     route: contactRoute,
+  },
+  {
+    path: "/contact-messages",
+    route: contactMessageRoute,
   },
  
 ];
