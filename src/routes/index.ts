68edaf6559a1140bad7b05181a5caeff44c60e5e
Arman Mia <mdarman365506@gmail.com>
2025-10-22 18:51:20 +0600

    Add and update Experience model with API

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import { projectroute } from "../modules/projects/projects.routes";
 import { skillRoute } from "../modules/skill/skill.routes";
 import { contactRoute } from "../modules/contact/contact.routes";
 import { contactMessageRoute } from "../modules/contactMessage/contactMessage.route";
+import { experienceRoutes } from "../modules/experiences/experience.route";
 
 
 export const router = Router();
@@ -34,7 +35,10 @@ const moduleRoutes = [
     path: "/contact-messages",
     route: contactMessageRoute,
   },
- 
+  {
+    path: "/experience",
+    route: experienceRoutes,
+  },
 ];
 
 moduleRoutes.forEach((route) => {
