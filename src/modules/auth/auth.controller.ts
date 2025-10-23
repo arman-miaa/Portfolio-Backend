52a9a5c25c2f716c4df4aa755270614b6e42c53c
Arman Mia <mdarman365506@gmail.com>
2025-10-23 23:33:14 +0600

    fix: resolved cookies error

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -48,11 +48,12 @@ const loginWithEmailAndPassword = async (req: Request, res: Response) => {
     // set cookie 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      
+      secure: true,
       // sameSite: "strict",
       sameSite: "none",
   
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 দিন
+      maxAge: 7 * 24 * 60 * 60 * 1000, 
     });
 
 
